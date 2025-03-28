b0d5a3a5e8eeacfd7ef54f5589edbb1a288bdcc5
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-28 19:46:25 +0900

    FEAT: 엑셀 파일을 읽는 함수 추가

--- src/main/kotlin/server/kkw/atmosphereInformation/service/ExcelService.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/service/ExcelService.kt
@@ -1,7 +1,23 @@
 package server.kkw.atmosphereInformation.service
 
+import org.apache.commons.io.FileExistsException
+import org.apache.poi.openxml4j.util.ZipSecureFile
+import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 
 @Service()
 class ExcelService {
+    fun read(fileName: String): Workbook {
+        val resource = ClassPathResource("data/$fileName")
+
+        if(!resource.exists()) {
+            throw FileExistsException();
+        }
+
+        ZipSecureFile.setMinInflateRatio(0.005)
+
+        return WorkbookFactory.create(resource.inputStream)
+    }
 }
