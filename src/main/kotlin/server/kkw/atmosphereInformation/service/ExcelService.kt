6d4e028df26a1c13728a63f6e13337371af8a695
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-28 20:07:05 +0900

    DOCS: 문서 주석 추가

--- src/main/kotlin/server/kkw/atmosphereInformation/service/ExcelService.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/service/ExcelService.kt
@@ -7,13 +7,21 @@ import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 
+/**
+ * 엑셀 파일을 쓰고/읽기 하는 서비스
+ */
 @Service()
 class ExcelService {
+    /**
+     * 특정 엑셀 파일의 워크북을 반환
+     * @param fileName 파일 이름
+     * @return Workbook
+     */
     fun read(fileName: String): Workbook {
         val resource = ClassPathResource("data/$fileName")
 
         if(!resource.exists()) {
-            throw FileExistsException();
+            throw FileExistsException()
         }
 
         ZipSecureFile.setMinInflateRatio(0.005)
