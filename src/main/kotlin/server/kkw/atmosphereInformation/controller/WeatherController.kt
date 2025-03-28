614f1c11e51738325589e421c52e118859206892
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-28 18:53:21 +0900

    FEAT: 날씨 API 기본 컨트롤러 생성

---
+++ src/main/kotlin/server/kkw/atmosphereInformation/controller/WeatherController.kt
@@ -0,0 +1,10 @@
+package server.kkw.atmosphereInformation.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/weather")
+class WeatherController {
+}
