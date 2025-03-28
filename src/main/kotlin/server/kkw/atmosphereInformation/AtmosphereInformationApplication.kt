c938a9f8370d77a92559a06fa193085747dfe934
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-28 18:50:57 +0900

    Initial commit

---
+++ src/main/kotlin/server/kkw/atmosphereInformation/AtmosphereInformationApplication.kt
@@ -0,0 +1,11 @@
+package server.kkw.atmosphereInformation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtmosphereInformationApplication
+
+fun main(args: Array<String>) {
+	runApplication<AtmosphereInformationApplication>(*args)
+}
---
+++ src/test/kotlin/server/kkw/atmosphereInformation/AtmosphereInformationApplicationTests.kt
@@ -0,0 +1,13 @@
+package server.kkw.atmosphereInformation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AtmosphereInformationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
