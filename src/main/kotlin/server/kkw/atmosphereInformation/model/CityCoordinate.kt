8841a92d5f1835ea57ff441e76c62941a3606924
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-28 21:26:20 +0900

    DOCS: CityCoordinate의 설명 추가

--- src/main/kotlin/server/kkw/atmosphereInformation/model/CityCoordinate.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/model/CityCoordinate.kt
@@ -1,3 +1,9 @@
 package server.kkw.atmosphereInformation.model
 
+/**
+ * 시도 좌표 데이터 클래스
+ * @property name 시도 이름
+ * @property x x 좌표
+ * @property y y 좌표
+ */
 data class CityCoordinate(val name: String, val x: Double, val y: Double)
