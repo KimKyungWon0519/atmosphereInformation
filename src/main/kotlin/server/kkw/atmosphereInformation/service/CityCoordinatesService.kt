668ebc98e1d48c50ee534c5de16e2da5e076d85d
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-28 20:04:46 +0900

    FEAT: CityCoordnates 엑셀 파일과 시트 데이터를 가져옴

--- src/main/kotlin/server/kkw/atmosphereInformation/service/CityCoordinatesService.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/service/CityCoordinatesService.kt
@@ -1,7 +1,23 @@
 package server.kkw.atmosphereInformation.service
 
+import jakarta.annotation.PostConstruct
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Workbook
 import org.springframework.stereotype.Service
 
 @Service
 class CityCoordinatesService(private val excelService: ExcelService) {
+    /**
+     * city_coordinates 엑셀 파일
+     */
+    private val workbook: Workbook by lazy {
+        excelService.read("city_coordinates.xlsx")
+    }
+
+    /**
+     *  city_coordinates의 첫번쨰 시트 데이
+     */
+    private val sheet: Sheet by lazy {
+        workbook.getSheetAt(0)
+    }
 }
