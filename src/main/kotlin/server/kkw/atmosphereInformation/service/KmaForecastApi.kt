e7f686581044b55cbfc5fece0c354a4a54419f2a
KimKyungWon0519 <gwon080870@gmail.com>
2025-04-01 20:14:23 +0900

    FEAT: 초단기 실황 API 연결

--- src/main/kotlin/server/kkw/atmosphereInformation/service/KmaForecastApi.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/service/KmaForecastApi.kt
@@ -1,4 +1,36 @@
 package server.kkw.atmosphereInformation.service
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import server.kkw.atmosphereInformation.model.KmaForecastResponse
+
+/**
+ * 기상청 단기예보 API
+ *
+ * [기상청_단기예보 ((구)_동네예보) 조회서비스](https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15084084)
+ */
 interface KmaForecastApi {
+    /**
+     * 초단기실황조회
+     *
+     * X, Y 좌표는 별첨 엑셀 좌표 확인
+     * @param serviceKey 인증키
+     * @param pageNo 한 페이지 결과
+     * @param baseData 발표일자
+     * @param baseTime 발표시각
+     * @param nx 예보지점 X 좌표
+     * @param ny 예보지점 Y 좌표
+     * @param dataType 응답형식자료
+     */
+    @GET("/getUltraSrtNcst")
+    fun getUltraSrtNcst(
+        @Query("serviceKey") serviceKey: String,
+        @Query("pageNo") pageNo: Int,
+        @Query("base_data") baseData: Long,
+        @Query("base_time") baseTime: Int,
+        @Query("nx") nx: Short,
+        @Query("ny") ny: Short,
+        @Query("dataType") dataType: String?,
+    ): Call<KmaForecastResponse>
 }
