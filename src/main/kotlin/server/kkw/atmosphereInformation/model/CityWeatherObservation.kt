672a11a96911a9d3f6568377858c75660d92d7e4
KimKyungWon0519 <gwon080870@gmail.com>
2025-04-08 20:48:53 +0900

    FEAT: 날씨 실황값을 저장하는 데이터 클래스 생성

---
+++ src/main/kotlin/server/kkw/atmosphereInformation/model/CityWeatherObservation.kt
@@ -0,0 +1,4 @@
+package server.kkw.atmosphereInformation.model
+
+data class CityWeatherObservation(val name: String, val observation: Set<Observation>)
+
---
+++ src/main/kotlin/server/kkw/atmosphereInformation/model/Observation.kt
@@ -0,0 +1,3 @@
+package server.kkw.atmosphereInformation.model
+
+data class Observation(val category: String, val data: Double)
