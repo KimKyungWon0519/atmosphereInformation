67b7bd07765d1d9eb7100771aecbb328a13eebaa
KimKyungWon0519 <gwon080870@gmail.com>
2025-05-08 19:12:31 +0900

    FEAT: 행정구역의 목록을 가져오는 Repository의 기본형 생성

---
+++ src/main/kotlin/server/kkw/atmosphereInformation/repository/RegionsRepository.kt
@@ -0,0 +1,10 @@
+package server.kkw.atmosphereInformation.repository
+
+import org.springframework.stereotype.Repository
+
+/**
+ * 행정구역(광역지방자치단체/기초지방자치단체)의 목록을 가져옴
+ */
+@Repository
+class RegionsRepository {
+}
