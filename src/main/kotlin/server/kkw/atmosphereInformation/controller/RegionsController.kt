c6b055c667ba4c619f76d577af7e5696b0d838c8
KimKyungWon0519 <gwon080870@gmail.com>
2025-05-08 19:07:09 +0900

    FEAT: 행정구역명을 가져오는 API 컨트롤러 기본형 생성

---
+++ src/main/kotlin/server/kkw/atmosphereInformation/controller/RegionsController.kt
@@ -0,0 +1,13 @@
+package server.kkw.atmosphereInformation.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+행정구역명을 가져오는 API 컨트롤러
+ */
+@RestController
+@RequestMapping("/v1/regions")
+class RegionsController {
+}
