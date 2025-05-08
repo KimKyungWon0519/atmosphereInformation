165ad0d9f514b310c59cd86a19321e2276f64908
KimKyungWon0519 <gwon080870@gmail.com>
2025-05-08 19:26:14 +0900

    FEAT: 모든 광역지방자치의 이름을 반환하는 기능 추가 [RegionsRepository.getAllMetropolitanNames]

--- src/main/kotlin/server/kkw/atmosphereInformation/repository/RegionsRepository.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/repository/RegionsRepository.kt
@@ -1,16 +1,22 @@
 package server.kkw.atmosphereInformation.repository
 
 import org.springframework.stereotype.Repository
+import server.kkw.atmosphereInformation.service.LocalGovernmentCoordinatesService
 
 /**
  * 행정구역(광역지방자치단체/기초지방자치단체)의 목록을 가져옴
  */
 @Repository
-class RegionsRepository {
+class RegionsRepository(private val localGovernmentCoordinatesService: LocalGovernmentCoordinatesService) {
     /**
      * 모든 광역지방자치단체의 이름을 반환함
+     *
+     * @return
+     * Set<String>
+     *
+     * 모든 광역지방자치단체의 이름
      */
-    fun getMetropolitan() {
-
+    fun getAllMetropolitanNames() : Set<String> {
+        return localGovernmentCoordinatesService.getAllMetropolitanNames()
     }
 }
