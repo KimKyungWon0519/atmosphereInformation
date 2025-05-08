4cdd9436dd82b50c04f753602ac10cedfc636616
KimKyungWon0519 <gwon080870@gmail.com>
2025-05-08 19:30:04 +0900

    FEAT: 모든 광역지방자치단체 이름을 반환 [RegionsController.getAllMetropolitanNames]

--- src/main/kotlin/server/kkw/atmosphereInformation/controller/RegionsController.kt
+++ src/main/kotlin/server/kkw/atmosphereInformation/controller/RegionsController.kt
@@ -1,20 +1,32 @@
 package server.kkw.atmosphereInformation.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import server.kkw.atmosphereInformation.repository.RegionsRepository
+import java.time.LocalDateTime
 
 /**
 행정구역명을 가져오는 API 컨트롤러
  */
 @RestController
 @RequestMapping("/v1/regions")
-class RegionsController() {
+class RegionsController(private val regionsRepository: RegionsRepository) {
     /**
      * 모든 광역지방자치단체 이름을 반환함
      */
     @GetMapping("/metropolitan")
-    fun getMetropolitan() : String {
-        return "Hello World"
+    fun getAllMetropolitanNames() : ResponseEntity<Map<String, Any>> {
+        val dateTime = LocalDateTime.now()
+        val metropolitanNames = regionsRepository.getAllMetropolitanNames()
+
+        return ResponseEntity.ok(
+            mapOf(
+                "time" to dateTime,
+                "status" to 200,
+                "data" to metropolitanNames
+            )
+        )
     }
 }
